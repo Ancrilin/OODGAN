183c3119616d1b7d91e0efb917bd97cb3dc4d399
Arcilin <1162236967@qq.com>
2021-01-27 11:50:56 +0800

    run sh

---
+++ run_sh/run_only_maxlen_gan.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+seeds="16 123 256 512 1024 1536 2048 4096 8192"
+
+# $1 maxlen
+
+for seed in ${seeds} ; do
+  python -m app.run_gan \
+  --seed=${seed}  \
+  --fine_tune \
+  --n_epoch=500  \
+  --patience=10 \
+  --D_lr=2e-5 \
+  --G_lr=2e-5 \
+  --bert_lr=4e-5  \
+  --n_epoch=500 \
+  --patience=10 \
+  --dataset=smp \
+  --data_file=binary_smp_full \
+  --bert_type=bert-base-chinese \
+  --do_train \
+  --do_eval \
+  --do_test \
+  --do_vis \
+  --output_dir=only-maxlen-gan/only-maxlen-gan-smp_${seed}  \
+  --maxlen=${1}
+
+done
+exit 0
