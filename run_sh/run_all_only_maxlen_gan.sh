774e3fede545ccbae67798d736c2d071158704ea
Arcilin <1162236967@qq.com>
2021-02-02 19:32:51 +0800

    maxlen 29

--- run_sh/run_all_only_maxlen_gan.sh
+++ run_sh/run_all_only_maxlen_gan.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-maxlens="17 18 19 20 21 22 23 24 25 26 27 28 30 31 32 33 34 35 36 37 38 39 40"
+maxlens="17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"
 
 for maxlen in ${maxlens} ; do
   bash run_sh/run_only_maxlen_gan.sh ${maxlen}
