eb8623d4b8a0c87748dce23002792457fe092fa7
Arcilin <1162236967@qq.com>
2021-02-21 10:18:23 +0800

    only maxlen 40->60

--- run_sh/run_all_only_maxlen_gan.sh
+++ run_sh/run_all_only_maxlen_gan.sh
@@ -1,23 +1,9 @@
 #! /bin/bash
 
-maxlens="17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"
+maxlens="41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60"
 
 for maxlen in ${maxlens} ; do
   bash run_sh/run_only_maxlen_gan.sh ${maxlen}
-
-done
-exit 0
-
-minlens="1 2 3 4 5"
-
-for minlen in ${minlens} ; do
-  bash run_sh/run_only_minlen_gan.sh ${minlen}
-
-done
-exit 0
-
-for minlen in ${minlens} ; do
-  bash run_sh/run_remove_entity_minlen_gan.sh ${minlen}
 
 done
 exit 0
